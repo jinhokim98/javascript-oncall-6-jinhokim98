47c7d94ee0867e3c53c101eb057e09f0b4d1fc0f
김진호 <rlawlsgh1227@gmail.com>
2023-12-16 14:20:26 +0900

    feat: 1년의 마지막 일 정의

---
+++ src/constants/condition.js
@@ -0,0 +1,20 @@
+const CONDITION = {
+  MONTH: {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  },
+};
+
+Object.freeze(CONDITION);
+
+export default CONDITION;
